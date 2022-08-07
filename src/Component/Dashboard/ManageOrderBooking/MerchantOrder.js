84a8090f0ba141f2729d3b6348905f40389d4283
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-08-07 23:21:03 +0600

    Create Merchant Page Design Complete

--- src/Component/Dashboard/ManageOrderBooking/MerchantOrder.js
+++ src/Component/Dashboard/ManageOrderBooking/MerchantOrder.js
@@ -159,374 +159,277 @@ const MerchantOrder = () => {
 			<Box sx={{ mx: 3 }}>
 				<form onSubmit={handleSubmit(onSubmit)}>
 					{/* Receiver Info Here */}
-					<Typography
-						component='p'
-						sx={{
-							fontWeight: "bold",
-							textAlign: "left",
-							my: 1,
-							mx: 2,
-							color: "#009688",
-							fontSize: "18px",
-						}}>
-						Receiver Information
-					</Typography>
-					<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
-						<TextField
-							size='small'
-							sx={{ my: 0.5 }}
-							fullWidth
-							required
-							label='Receiver Name'
-							helperText='Receiver Name'
-							{...register("receiverName", { required: true })}
-						/>
-						<TextField
-							size='small'
-							type='number'
-							sx={{ my: 0.5 }}
-							fullWidth
-							required
-							label='Mobile Number'
-							helperText='Reciever Mobile Number'
-							{...register("receiverNumber", { required: true })}
-						/>
-					</Box>
-					<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
-						<Autocomplete
-							onChange={(event, newValue) => {
-								setSelectedDistricts(newValue);
-							}}
-							size='small'
-							sx={{ my: 1, width: "100% !important" }}
-							options={branch}
-							getOptionLabel={(option) => option?.branchDistrict}
-							style={{ width: 300 }}
-							renderInput={(params) => (
-								<TextField
-									{...register("branchDistrict", { required: true })}
-									{...params}
-									label='Districts Name'
-									variant='outlined'
+					<Box sx={{ display: { sm: "block", md: "block", lg: "flex" }, justifyContent: "space-between", gap: 4 }}>
+						<Box sx={{ flexGrow: 2 }}>
+							{/* Order Info Here */}
+							<Typography
+								component='p'
+								sx={{
+									fontWeight: "bold",
+									textAlign: "left",
+									my: 1,
+									mx: 2,
+									color: "#009688",
+									fontSize: "18px",
+								}}>
+								Order Information
+							</Typography>
+							<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
+								<Autocomplete
+									size='small'
+									sx={{ my: 0.5, width: "100% !important" }}
+									options={productCategory}
+									getOptionLabel={(option) => option.itemCategoryName}
+									style={{ width: 300 }}
+									renderInput={(params) => (
+										<TextField
+											{...register("productCategory", { required: true })}
+											{...params}
+											label='Product Category'
+											variant='outlined'
+											helperText='Select Product category'
+										/>
+									)}
 								/>
-							)}
-						/>
-						<Autocomplete
-							onChange={(event, newValue) => {
-								setSelectedBranch(newValue);
-							}}
-							size='small'
-							sx={{ my: 0.5, width: "100% !important" }}
-							options={branch?.filter(
-								(item) =>
-									item?.branchDistrict === selectedDistricts?.branchDistrict,
-							)}
-							getOptionLabel={(option) => option.branchName}
-							style={{ width: 300 }}
-							renderInput={(params) => (
-								<TextField
-									{...register("merchantBranchName", { required: true })}
-									{...params}
-									label='Select Branch'
-									variant='outlined'
-									helperText='Branch'
+								<Autocomplete
+									onChange={(event, newValue) => {
+										setSelectWeight(newValue);
+									}}
+									size='small'
+									sx={{ my: 0.5, width: "100% !important" }}
+									options={weight}
+									getOptionLabel={(option) => option.weightPackageName}
+									style={{ width: 300 }}
+									renderInput={(params) => (
+										<TextField
+											{...register("productWeight", { required: true })}
+											{...params}
+											label='Product Weight'
+											variant='outlined'
+											helperText='Select Product Weight'
+										/>
+									)}
 								/>
-							)}
-						/>
-					</Box>
-					<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
-						<Autocomplete
-							size='small'
-							sx={{ my: 0.5, width: "100% !important" }}
-							options={selectedBranch?.branchArea || []}
-							getOptionLabel={(option) => option.area}
-							style={{ width: 300 }}
-							renderInput={(params) => (
-								<TextField
-									{...register("merchantArea", { required: true })}
-									{...params}
-									label='Select Area'
-									variant='outlined'
-									helperText='Area'
+								<Autocomplete
+									onChange={(event, newValue) => {
+										setSelectServiceAreas(newValue);
+									}}
+									size='small'
+									sx={{ my: 0.5, width: "100% !important" }}
+									options={serviceAreas || []}
+									getOptionLabel={(option) => option.serviceAreaName}
+									style={{ width: 300 }}
+									renderInput={(params) => (
+										<TextField
+											{...register("productType", { required: true })}
+											{...params}
+											label='Service Area'
+											variant='outlined'
+											helperText='Select Service Area'
+										/>
+									)}
 								/>
-							)}
-						/>
-						<TextField
-							size='small'
-							sx={{ my: 0.5 }}
-							fullWidth
-							required
-							label='Address'
-							helperText='Address'
-							{...register("receiverAddress", { required: true })}
-						/>
-					</Box>
-					{/* Order Info Here */}
-					<Typography
-						component='p'
-						sx={{
-							fontWeight: "bold",
-							textAlign: "left",
-							my: 1,
-							mx: 2,
-							color: "#009688",
-							fontSize: "18px",
-						}}>
-						Order Information
-					</Typography>
-					<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
-						<Autocomplete
-							size='small'
-							sx={{ my: 0.5, width: "100% !important" }}
-							options={productCategory}
-							getOptionLabel={(option) => option.itemCategoryName}
-							style={{ width: 300 }}
-							renderInput={(params) => (
-								<TextField
-									{...register("productCategory", { required: true })}
-									{...params}
-									label='Product Category'
-									variant='outlined'
-									helperText='Select Product category'
-								/>
-							)}
-						/>
-						<Autocomplete
-							onChange={(event, newValue) => {
-								setSelectWeight(newValue);
-							}}
-							size='small'
-							sx={{ my: 0.5, width: "100% !important" }}
-							options={weight}
-							getOptionLabel={(option) => option.weightPackageName}
-							style={{ width: 300 }}
-							renderInput={(params) => (
-								<TextField
-									{...register("productWeight", { required: true })}
-									{...params}
-									label='Product Weight'
-									variant='outlined'
-									helperText='Select Product Weight'
-								/>
-							)}
-						/>
+							</Box>
+							<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
 
-						<Autocomplete
-							onChange={(event, newValue) => {
-								setSelectServiceAreas(newValue);
-							}}
-							size='small'
-							sx={{ my: 0.5, width: "100% !important" }}
-							options={serviceAreas || []}
-							getOptionLabel={(option) => option.serviceAreaName}
-							style={{ width: 300 }}
-							renderInput={(params) => (
 								<TextField
-									{...register("productType", { required: true })}
-									{...params}
-									label='Service Area'
-									variant='outlined'
-									helperText='Select Service Area'
+									type='number'
+									size='small'
+									sx={{ my: 0.5 }}
+									fullWidth
+									required
+									label='Cash Collection'
+									helperText='Cash Collection'
+									{...register("cashCollection", { required: true })}
+									value={cashCollection}
+									onChange={(e) => setCashCollection(e.target.value)}
 								/>
-							)}
-						/>
+								<TextField
+									size='small'
+									sx={{ my: 0.5 }}
+									fullWidth
+									required
+									label='Reference Id'
+									helperText='Reference Id'
+									{...register("referenceId", { required: true })}
+								/>
+								<TextField
+									size='small'
+									sx={{ my: 0.5 }}
+									fullWidth
+									label='Instructions'
+									multiline
+									rows={1}
+									helperText='Any Instructions'
+									{...register("instructions", { required: true })}
+								/>
+							</Box>
+							<FormGroup sx={{ my: 0.5, minWidth: "230px", mx: 2.5 }}>
+								<FormControlLabel
+									control={
+										<Checkbox
+											checked={addDeliveryCharge}
+											onChange={(e) => setAddDeliveryCharge(e.target.checked)}
+										/>
+									}
+									label='With Delivery Charge'
+								/>
+							</FormGroup>
+						</Box>
+						<Box>
+							<Typography
+								component='p'
+								sx={{
+									fontWeight: "bold",
+									textAlign: "left",
+									my: 1,
+									mx: 2,
+									color: "#009688",
+									fontSize: "18px",
+								}}>
+								Order Summary
+							</Typography>
+							<TableContainer component={Paper}>
+								<Table aria-label='simple table'>
+									<TableBody>
+										<TableRow>
+											<TableCell component='th' scope='row'>
+												<Typography
+													component='p'
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													Cash Collection
+												</Typography>
+											</TableCell>
+											<TableCell align='right'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													{cashCollected} Taka
+												</Typography>
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell component='th' scope='row'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													Weight Charge
+												</Typography>
+											</TableCell>
+											<TableCell align='right'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													{weightCharge} Taka
+												</Typography>
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell component='th' scope='row'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													Delivery Charge
+												</Typography>
+											</TableCell>
+											<TableCell align='right'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													{deliveryCharge || 0} Taka
+												</Typography>
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell component='th' scope='row'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													Cash On Delivery Charge
+												</Typography>
+											</TableCell>
+											<TableCell align='right'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													{codAmount} Taka
+												</Typography>
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell component='th' scope='row'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													Total
+												</Typography>
+											</TableCell>
+											<TableCell align='right'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													{totalPayment} Taka
+												</Typography>
+											</TableCell>
+										</TableRow>
+										<TableRow style={{ background: "#e9e9e9" }}>
+											<TableCell component='th' scope='row'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													Payable Amount
+												</Typography>
+											</TableCell>
+											<TableCell align='right'>
+												<Typography
+													sx={{
+														fontWeight: "600",
+														color: "gray",
+														fontSize: "15px",
+													}}>
+													{payment} Taka
+												</Typography>
+											</TableCell>
+										</TableRow>
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</Box>
 					</Box>
-					<Box sx={{ display: "flex", gap: "15px", mx: 2 }}>
-						<FormGroup sx={{ my: 0.5, minWidth: "230px" }}>
-							<FormControlLabel
-								control={
-									<Checkbox
-										checked={addDeliveryCharge}
-										onChange={(e) => setAddDeliveryCharge(e.target.checked)}
-									/>
-								}
-								label='With Delivery Charge'
-							/>
-						</FormGroup>
-						<TextField
-							type='number'
-							size='small'
-							sx={{ my: 0.5 }}
-							fullWidth
-							required
-							label='Cash Collection'
-							helperText='Cash Collection'
-							{...register("cashCollection", { required: true })}
-							value={cashCollection}
-							onChange={(e) => setCashCollection(e.target.value)}
-						/>
-						<TextField
-							size='small'
-							sx={{ my: 0.5 }}
-							fullWidth
-							required
-							label='Reference Id'
-							helperText='Reference Id'
-							{...register("referenceId", { required: true })}
-						/>
-					</Box>
-					<TextField
-						size='small'
-						sx={{ my: 0.5 }}
-						fullWidth
-						label='Instructions'
-						multiline
-						rows={3}
-						helperText='Any Instructions'
-						{...register("instructions", { required: true })}
-					/>
-					<Typography
-						component='p'
-						sx={{
-							fontWeight: "bold",
-							textAlign: "left",
-							my: 1,
-							mx: 2,
-							color: "#009688",
-							fontSize: "18px",
-						}}>
-						Order Summary
-					</Typography>
-					<TableContainer component={Paper}>
-						<Table aria-label='simple table'>
-							<TableBody>
-								<TableRow>
-									<TableCell component='th' scope='row'>
-										<Typography
-											component='p'
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											Cash Collection
-										</Typography>
-									</TableCell>
-									<TableCell align='right'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											{cashCollected} Taka
-										</Typography>
-									</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell component='th' scope='row'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											Weight Charge
-										</Typography>
-									</TableCell>
-									<TableCell align='right'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											{weightCharge} Taka
-										</Typography>
-									</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell component='th' scope='row'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											Delivery Charge
-										</Typography>
-									</TableCell>
-									<TableCell align='right'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											{deliveryCharge || 0} Taka
-										</Typography>
-									</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell component='th' scope='row'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											Cash On Delivery Charge
-										</Typography>
-									</TableCell>
-									<TableCell align='right'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											{codAmount} Taka
-										</Typography>
-									</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell component='th' scope='row'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											Total
-										</Typography>
-									</TableCell>
-									<TableCell align='right'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											{totalPayment} Taka
-										</Typography>
-									</TableCell>
-								</TableRow>
-								<TableRow style={{ background: "#e9e9e9" }}>
-									<TableCell component='th' scope='row'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											Payable Amount
-										</Typography>
-									</TableCell>
-									<TableCell align='right'>
-										<Typography
-											sx={{
-												fontWeight: "600",
-												color: "gray",
-												fontSize: "15px",
-											}}>
-											{payment} Taka
-										</Typography>
-									</TableCell>
-								</TableRow>
-							</TableBody>
-						</Table>
-					</TableContainer>
-
 					<Box sx={{ display: "flex", gap: "15px", mx: 1, mt: 2 }}>
 						<Button
 							type='submit'
