432f160c834b894363dd3a26d8f2a9b5335e0d06
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-10-02 01:14:59 +0600

    Partners Page Design Complete

--- src/Component/HomeComponnents/Partners/Partners.js
+++ src/Component/HomeComponnents/Partners/Partners.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import A2i from "../../../Assets/Image/partner/A2i.png";
 import DarulHikmah from "../../../Assets/Image/partner/DarulHikmah.png";
 import DuaInt from "../../../Assets/Image/partner/DuaInt.png";
@@ -23,29 +23,32 @@ const Partners = () => {
   ]
 
   return (
-    <Container>
-      <h2
-        style={{
-          fontFamily: "Montserrat, sans-serif",
-          fontSize: 30,
-          textAlign: "start",
-          fontWeight: 700,
-          color: "#08A74C",
-          marginBottom: 10,
-          marginTop: 5
-        }}>
-        Our Partners
-      </h2>
-      <div style={{ height: 120 }}>
-        <Marquee velocity={35}>
-          {times(7, Number).map(id => (
-            <img src={images[id]} key={`marquee-example-people-${id}`} alt="Slider" style={{
-              marginLeft: "50px", width: "45%", height: "45%"
-            }} />
-          ))}
-        </Marquee>
-      </div>
-    </Container>
+    <Box className="partnerContainer">
+      <Container sx={{ position: "relative", py: 5 }}>
+        <p className="partnerDivider" style={{ textAlign: "left", color: "#cfcfcf", fontWeight: 600, marginBottom: "8px" }}>TOP BRANDS</p>
+        <h2
+          style={{
+            textAlign: "start",
+            fontWeight: 600,
+            fontFamily: "Montserrat, sans-serif",
+            fontSize: 27,
+            marginBottom: "30px",
+            color: "white"
+          }}
+        >
+          Meet Our <span style={{ color: "#f4ad2b" }}>Partners</span>
+        </h2>
+        <div style={{ height: 130 }}>
+          <Marquee velocity={35}>
+            {times(7, Number).map(id => (
+              <img src={images[id]} key={`marquee-example-people-${id}`} alt="Slider" style={{
+                margin: "0px 60px", width: "80px", height: "80px", borderRadius: "5px",
+              }} />
+            ))}
+          </Marquee>
+        </div>
+      </Container>
+    </Box>
   );
 };
 
