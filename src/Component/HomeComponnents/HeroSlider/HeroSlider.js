64484019876c2be795cf829812b3b87b519ebb40
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-10-10 22:40:22 +0600

    New Footer Design Added

--- src/Component/HomeComponnents/HeroSlider/HeroSlider.js
+++ src/Component/HomeComponnents/HeroSlider/HeroSlider.js
@@ -1,29 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
-/* import { Swiper, SwiperSlide } from "swiper/react";
-import sliderimg from "../../../Assets/Image/sliderimg.png";
-import sliderimg2 from "../../../Assets/Image/slide3.jpg";
-import sliderimg3 from "../../../Assets/Image/slide4.jpg";
-import sliderimg4 from "../../../Assets/Image/slide5.png"; */
-
-// Import Swiper styles
-/* import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
-import "swiper/css/zoom"; */
-
-// import required modules
-/* import SwiperCore, {
-  Pagination,
-  Mousewheel,
-  Keyboard,
-  Autoplay
-} from "swiper";
-*/
 import "./HeroSlider.css";
 const HeroSlider = () => {
-  // SwiperCore.use([Autoplay]);
   return (
     <Box style={{ height: "75vh" }}>
       <div className="slider-section">
@@ -48,42 +26,6 @@ const HeroSlider = () => {
         </div>
         <div className="sliderStyle"></div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      {/* <Swiper
-        cssMode={true}
-        mousewheel={true}
-        keyboard={true}
-        loop={true}
-        autoplay={true}
-        modules={[Pagination, Mousewheel, Keyboard, Autoplay]}
-        className="heroSwiper">
-        <SwiperSlide>
-          <img src={sliderimg} alt="sliderimg" className="sliderimg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={sliderimg2} alt="sliderimg" className="sliderimg" />
-        </SwiperSlide >
-        <SwiperSlide>
-          <img src={sliderimg3} alt="sliderimg" className="sliderimg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={sliderimg4} alt="sliderimg" className="sliderimg" />
-        </SwiperSlide>
-      </Swiper> */}
     </Box>
   );
 };
--- src/Component/Shared/Footer/Footer.js
+++ src/Component/Shared/Footer/Footer.js
@@ -13,213 +13,207 @@ import EmailIcon from "@mui/icons-material/Email";
 import DirectionsIcon from "@mui/icons-material/Directions";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import shapeImage from "../../../Assets/Image/shape4.png";
 
 const Footer = () => {
-	/*   const [hide, setHide] = useState("block");
-  const location = useLocation();
-  useEffect(() => {
-	if (location?.pathname.includes("/")) {
-	  setHide("block");
-	}
-	if (location?.pathname.includes("/home")) {
-	  setHide("block");
-	}
-	if (location?.pathname.includes("/dashboard")) {
-	  setHide("none");
-	}
-  }, [location.pathname]); */
+
 	const location = useLocation();
 	if (location?.pathname.includes("/dashboard")) {
 		return false;
 	}
 
 	return (
-		<Box className='footerContainer' >
-			<Box sx={{ backgroundColor: "#00283ede" }}>
-				<Box>
-					<Grid
-						container
-						spacing={3}
-						sx={{
-							pl: { md: 10, xs: 5 },
-							color: "white",
-							pt: { md: 8, sm: 2, xs: 2 },
-							pb: { md: 10, sm: 2, xs: 2 },
-						}}>
-						<Grid item xs={12} md={3}>
-							<Box>
-								<img src={footerlogo} width={180} height={80} alt='logo' />
-							</Box>
-							{/* map */}
-							<Box style={{ marginTop: "12px" }}>
-								<div className='gmap_canvas'>
-									<iframe
-										style={{ borderRadius: "5px" }}
-										title='map'
-										width='330'
-										height='204'
-										id='gmap_canvas'
-										src='https://maps.google.com/maps?q=89/123%20,(%20Manik%20Nagar%20Bishwo%20Road,%20Ram%20Krishna%20Mission%20Rd,%20Dhaka%201203&t=&z=13&ie=UTF8&iwloc=&output=embed'
-										frameBorder='0'
-										scrolling='no'
-										marginHeight='0'
-										marginWidth='0'></iframe>
-								</div>
-							</Box>
-						</Grid>
-
-						<Grid item xs={12} md={2}>
-							<h4 className='footerMenuHeading'>Contact Us</h4>
-							<hr className="hrDesign" />
-							<h4 className='contactInfo'>
-								<CallIcon sx={{ pr: 1 }} />
-								Call us: +8809613829867
-							</h4>
-							<h4 className='contactInfo'>
-								<EmailIcon sx={{ pr: 1 }} />
-								info@alijahan.com
-							</h4>
-							<h4 className='contactInfo'>
-								<DirectionsIcon sx={{ pr: 1 }} />
-								89/123 ,( Manik Nagar Bishwo Road, Ram Krishna Mission Rd, Dhaka
-								1203
-							</h4>
-							<Box sx={{ width: { md: "40%", xs: "90%" } }}>
-								<Box
-									sx={{
-										display: { md: "flex", xs: "block" },
-										alignItems: "center",
-									}}>
-									<FacebookIcon className='socialIcon' />
-									<WhatsAppIcon className='socialIcon' />
-									<InstagramIcon className='socialIcon' />
-									<LinkedInIcon className='socialIcon' />
+		<>
+			<footer style={{ backgroundColor: "#fbfbfd" }}>
+				<div className="new_footer_top" style={{ position: "relative" }}>
+					<Box>
+						<Grid
+							container
+							spacing={3}
+							sx={{
+								pl: { md: 10, xs: 5 },
+								color: "white",
+								pt: { md: 4, sm: 2, xs: 2 }
+							}}>
+							<Grid item xs={12} md={3}>
+								<Box>
+									<img src={footerlogo} width={180} height={80} alt='logo' />
 								</Box>
-							</Box>
-						</Grid>
-						{/* menu about */}
-						<Grid item xs={12} md={2}>
-							<h4 className='footerMenuHeading'>About Us</h4>
-							<hr className="hrDesign" />
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Tracking</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Coverage Map</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>FAQs</Box>
-								</p>
-							</Link>
-							<Link to='/privacypolicy' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Privacy Policy</Box>
-								</p>
-							</Link>
-							<Link to='/termscondition' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Terms & Conditions</Box>
-								</p>
-							</Link>
-							<Link to='/returnrefundpolicy' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Return Policy</Box>
-								</p>
-							</Link>
-						</Grid>
-						{/* menu service */}
-						<Grid item xs={12} md={2}>
-							<h4 className='footerMenuHeading'>Services</h4>
-							<hr className="hrDesign" />
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>E-Commerce</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Logistics</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Pick & Drop</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Warehouse</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Blogs</Box>
-								</p>
-							</Link>
-						</Grid>
-						{/* menu How We Can Help You */}
-						<Grid item xs={12} md={2}>
-							<h4 className='footerMenuHeading'>How We Can Help You</h4>
-							<hr className="hrDesign" />
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Contact Us</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Customer Care</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Career</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Local Notice</Box>
-								</p>
-							</Link>
-							<Link to='/' className='footerMenuLink'>
-								<p style={{ display: "flex", alignItems: "center" }}>
-									<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
-									<Box>Festival Notice</Box>
-								</p>
-							</Link>
-						</Grid>
-					</Grid>
 
-					<Paper
-						evaluation={3}
-						className='goToTopBtn'
-						onClick={() => {
-							window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-						}}>
-						<ArrowUpwardIcon sx={{ textAlign: "right" }} />
-					</Paper>
+								<Box style={{ marginTop: "12px" }}>
+									<div className='gmap_canvas'>
+										<iframe
+											style={{ borderRadius: "5px" }}
+											title='map'
+											width='330'
+											height='204'
+											id='gmap_canvas'
+											src='https://maps.google.com/maps?q=89/123%20,(%20Manik%20Nagar%20Bishwo%20Road,%20Ram%20Krishna%20Mission%20Rd,%20Dhaka%201203&t=&z=13&ie=UTF8&iwloc=&output=embed'
+											frameBorder='0'
+											scrolling='no'
+											marginHeight='0'
+											marginWidth='0'></iframe>
+									</div>
+								</Box>
+							</Grid>
 
+							<Grid item xs={12} md={2}>
+								<h4 className='footerMenuHeading'>Contact Us</h4>
+								<hr className="hrDesign" />
+								<h4 className='contactInfo'>
+									<CallIcon sx={{ pr: 1 }} />
+									Call us: +8809613829867
+								</h4>
+								<h4 className='contactInfo'>
+									<EmailIcon sx={{ pr: 1 }} />
+									info@alijahan.com
+								</h4>
+								<h4 className='contactInfo'>
+									<DirectionsIcon sx={{ pr: 1 }} />
+									89/123 ,( Manik Nagar Bishwo Road, Ram Krishna Mission Rd, Dhaka
+									1203
+								</h4>
+								<Box sx={{ width: { md: "40%", xs: "90%" } }}>
+									<Box
+										sx={{
+											display: { md: "flex", xs: "block" },
+											alignItems: "center",
+										}}>
+										<FacebookIcon className='socialIcon' />
+										<WhatsAppIcon className='socialIcon' />
+										<InstagramIcon className='socialIcon' />
+										<LinkedInIcon className='socialIcon' />
+									</Box>
+								</Box>
+							</Grid>
+
+							<Grid item xs={12} md={2}>
+								<h4 className='footerMenuHeading'>About Us</h4>
+								<hr className="hrDesign" />
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Tracking</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Coverage Map</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>FAQs</Box>
+									</p>
+								</Link>
+								<Link to='/privacypolicy' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Privacy Policy</Box>
+									</p>
+								</Link>
+								<Link to='/termscondition' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Terms & Conditions</Box>
+									</p>
+								</Link>
+								<Link to='/returnrefundpolicy' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Return Policy</Box>
+									</p>
+								</Link>
+							</Grid>
+
+							<Grid item xs={12} md={2}>
+								<h4 className='footerMenuHeading'>Services</h4>
+								<hr className="hrDesign" />
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>E-Commerce</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Logistics</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Pick & Drop</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Warehouse</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Blogs</Box>
+									</p>
+								</Link>
+							</Grid>
+
+							<Grid item xs={12} md={2}>
+								<h4 className='footerMenuHeading'>How We Can Help You</h4>
+								<hr className="hrDesign" />
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Contact Us</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Customer Care</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Career</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Local Notice</Box>
+									</p>
+								</Link>
+								<Link to='/' className='footerMenuLink'>
+									<p style={{ display: "flex", alignItems: "center" }}>
+										<KeyboardDoubleArrowRightIcon sx={{ pr: "2px" }} />
+										<Box>Festival Notice</Box>
+									</p>
+								</Link>
+							</Grid>
+						</Grid>
+
+						<Paper
+							evaluation={3}
+							className='goToTopBtn'
+							onClick={() => {
+								window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+							}}>
+							<ArrowUpwardIcon sx={{ textAlign: "right" }} />
+						</Paper>
+					</Box>
+
+					<div className="footer_bg">
+						<div className="footer_bg_one"></div>
+						<div className="footer_bg_two"></div>
+					</div>
+				</div>
+				<div style={{ paddingTop: "20px", }}>
 					<Box
 						sx={{
 							borderTop: "1px solid #565656",
@@ -234,9 +228,9 @@ const Footer = () => {
 							</span>
 						</p>
 					</Box>
-				</Box>
-			</Box>
-		</Box>
+				</div>
+			</footer>
+		</>
 	);
 };
 
