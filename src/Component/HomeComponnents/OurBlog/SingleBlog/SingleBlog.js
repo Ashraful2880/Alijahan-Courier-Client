ef683902bb7041a90a4432090fe3e39a17307f1c
Ashraful Islam <ashraful.islam@aamartaka.com>
2023-05-14 00:33:52 +0600

    Blog Page Done

--- src/Component/HomeComponnents/OurBlog/SingleBlog/SingleBlog.js
+++ src/Component/HomeComponnents/OurBlog/SingleBlog/SingleBlog.js
@@ -89,7 +89,7 @@ const SingleBlog = () => {
             <CardContent>
               <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left", fontSize: "22px", fontWeight: 600, }}>
                 <span>
-                  Agreement with Faiha’s Style
+                  How To Book a Parcel
                 </span>
               </Typography>
               <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left", fontSize: "16px", }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -136,7 +136,149 @@ const SingleBlog = () => {
             <CardContent>
               <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left", fontSize: "22px", fontWeight: 600, }}>
                 <span>
-                  Agreement with Faiha’s Style
+                  How We Deliver Your Parcel
+                </span>
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left", fontSize: "16px", }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Saepe, in!This is Trustinternational Test Blog......
+              </Typography>
+            </CardContent>
+            <CardActions style={{ position: "absolute", bottom: "0%", width: "100%" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%", ml: 1, mr: 4 }}>
+                <Box>
+                  <Button size="small" sx={{ fontSize: "14px", color: "green", borderBottom: "2px solid white", borderRadius: "0px", "&:hover": { backgroundColor: "white", borderBottom: "2px solid green" } }}>
+                    Learn More  <ArrowForwardIcon style={{ fontSize: "18px", marginLeft: "10px" }} />
+                  </Button>
+                </Box>
+                <Box>
+                  <ShareIcon sx={{ color: "#2b2c41", cursor: "pointer", transition: "0.5s all", "&:hover": { color: "green" } }} />
+                </Box>
+              </Box>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} style={{marginTop:"10px"}}>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ width: "100%", height: 470, position: "relative" }} className="blogCard">
+            <CardMedia
+              component="img"
+              height="240"
+              image={blogimg1}
+              alt="BlogImage"
+              className="blogImage" />
+            <Box sx={{ backgroundColor: "#2B2C41", height: "45px", width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0px 20px", overflow: "hidden", zIndex: 999 }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}>
+                <CalendarMonthIcon sx={{ color: "white", fontSize: "20px", mr: 1, mt: "2px" }} />
+                <Typography variant="p" component="div" sx={{ color: "white" }}>
+                  17 Jan 2022
+                </Typography>
+              </Box>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}>
+                <MapsUgcIcon sx={{ color: "white", fontSize: "20px", mr: 1, mt: "2px" }} />
+                <Typography variant="p" component="div" sx={{ color: "white" }}>
+                  09 Comments
+                </Typography>
+              </Box>
+            </Box>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left", fontSize: "22px", fontWeight: 600, }}>
+                <span>
+                  Best Deliver Award 2022
+                </span>
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left", fontSize: "16px", }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Saepe, in!This is Trustinternational Test Blog......
+              </Typography>
+            </CardContent>
+            <CardActions style={{ position: "absolute", bottom: "0%", width: "100%" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%", ml: 1, mr: 4 }}>
+                <Box>
+                  <Button size="small" sx={{ fontSize: "14px", color: "green", borderBottom: "2px solid white", borderRadius: "0px", "&:hover": { backgroundColor: "white", borderBottom: "2px solid green" } }}>
+                    Learn More  <ArrowForwardIcon style={{ fontSize: "18px", marginLeft: "10px" }} />
+                  </Button>
+                </Box>
+                <Box>
+                  <ShareIcon sx={{ color: "#2b2c41", cursor: "pointer", transition: "0.5s all", "&:hover": { color: "green" } }} />
+                </Box>
+              </Box>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* 2nd */}
+        <Grid item xs={12} md={4}>
+          <Card sx={{ width: "100%", height: 470, position: "relative" }} className="blogCard" >
+            <CardMedia
+              component="img"
+              height="240"
+              image={blogimg2}
+              alt="BlogImage"
+              className="blogImage" />
+            <Box sx={{ backgroundColor: "#2B2C41", height: "45px", width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0px 20px" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}>
+                <CalendarMonthIcon sx={{ color: "white", fontSize: "20px", mr: 1, mt: "2px" }} />
+                <Typography variant="p" component="div" sx={{ color: "white" }}>
+                  21 Apr 2022
+                </Typography>
+              </Box>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}>
+                <MapsUgcIcon sx={{ color: "white", fontSize: "20px", mr: 1, mt: "2px" }} />
+                <Typography variant="p" component="div" sx={{ color: "white" }}>
+                  21 Comments
+                </Typography>
+              </Box>
+            </Box>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left", fontSize: "22px", fontWeight: 600, }}>
+                <span>
+                  Booking Commissions 2022
+                </span>
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left", fontSize: "16px", }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Saepe, in!This is Trustinternational Test Blog......
+              </Typography>
+            </CardContent>
+            <CardActions style={{ position: "absolute", bottom: "0%", width: "100%" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%", ml: 1, mr: 4 }}>
+                <Box>
+                  <Button size="small" sx={{ fontSize: "14px", color: "green", borderBottom: "2px solid white", borderRadius: "0px", "&:hover": { backgroundColor: "white", borderBottom: "2px solid green" } }}>
+                    Learn More  <ArrowForwardIcon style={{ fontSize: "18px", marginLeft: "10px" }} />
+                  </Button>
+                </Box>
+                <Box>
+                  <ShareIcon sx={{ color: "#2b2c41", cursor: "pointer", transition: "0.5s all", "&:hover": { color: "green" } }} />
+                </Box>
+              </Box>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* 3rd */}
+        <Grid item xs={12} md={4}>
+          <Card sx={{ width: "100%", height: 470, position: "relative" }} className="blogCard">
+            <CardMedia
+              component="img"
+              height="240"
+              image={blogimg3}
+              alt="BlogImage"
+              className="blogImage" />
+            <Box sx={{ backgroundColor: "#2B2C41", height: "45px", width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0px 20px" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}>
+                <CalendarMonthIcon sx={{ color: "white", fontSize: "20px", mr: 1, mt: "2px" }} />
+                <Typography variant="p" component="div" sx={{ color: "white" }}>
+                  06 Aug 2022
+                </Typography>
+              </Box>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}>
+                <MapsUgcIcon sx={{ color: "white", fontSize: "20px", mr: 1, mt: "2px" }} />
+                <Typography variant="p" component="div" sx={{ color: "white" }}>
+                  32 Comments
+                </Typography>
+              </Box>
+            </Box>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: "left", fontSize: "22px", fontWeight: 600, }}>
+                <span>
+                  International Award
                 </span>
               </Typography>
               <Typography variant="body2" color="text.secondary" sx={{ textAlign: "left", fontSize: "16px", }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
